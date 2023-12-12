85f61295a6e12be11c94a8e3aaa2b13ce6a05de4
Natalie Gillam <ngillam@hotmail.co.uk>
2023-12-12 22:44:39 +0000

    added new questions; added functions to get a random question number and display the text of the randomly selected question

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const questionNumber = document.querySelector(".question-number");
+
+const questionText = document.querySelector(".question-text");
+
+const optionContainer = document.querySelector(".option-container");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+
+// add the questions to the availableQuestions array
+function setAvailableQuestions() {
+  const totalQuestions = questions.length;
+  for (let i = 0; i < totalQuestions; i++) {
+    availableQuestions.push(questions[i]);
+  }
+}
+
+//set question number, question text and answer options
+function getNewQuestion(){
+    //set question number
+    questionNumber.innerHTML = `Question ${questionCounter + 1} of ${questions.length}`;
+    
+    //set question text
+    //get random question
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+    questionText.innerHTML = questionIndex.q;
+    console.log(questionText);
+}
+
+window.onload = function () {
+  setAvailableQuestions();
+  getNewQuestion();
+};
---
+++ js/questions.js
@@ -0,0 +1,42 @@
+const questions = [
+  {
+    id: 1,
+    q: "La matière",
+    options: ["The manner", "The subject", "The Earth", "The Maths"],
+    answer: 1,
+  },
+  {
+    id: 2,
+    q: "Je crois que",
+    options: ["I create", "I am craving", "I believe that", "I cross the..."],
+    answer: 1,
+  },
+
+  {
+    id: 3,
+    q: "Court",
+    options: ["Short", "Cars", "Cord", "Shard"],
+    answer: 1,
+  },
+
+  {
+    id: 4,
+    q: "Causé par",
+    options: ["Cured by", "Caused by", "Caused per", "Cursed by"],
+    answer: 1,
+  },
+
+  {
+    id: 5,
+    q: "Le boeuf",
+    options: ["The beef", "The buffer", "The best", "The boast"],
+    answer: 1,
+  },
+
+  {
+    id: 6,
+    q: "L'arrière petite fille (f)",
+    options: ["Behind the little daughter", "Great niece", "Great granddaughter", "Granddaughter"],
+    answer: 1,
+  },
+];
