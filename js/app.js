b1696f5bcd519a201f11c0d1ab4bc4109b22170f
Natalie Gillam <ngillam@hotmail.co.uk>
2023-12-16 17:23:19 +0000

    re-set options upon appearance of a new question

--- js/app.js
+++ js/app.js
@@ -27,7 +27,6 @@ function getNewQuestion() {
   //get random question
   const questionIndex =
     availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
-
   questionText.innerHTML = questionIndex.q;
 
   // get the position of "QuestionIndex" from the "AvailableQuestions" array
@@ -37,50 +36,49 @@ function getNewQuestion() {
   //remove questionIndex from the ‘AvailableQuestions’ array
 
   availableQuestions.splice(index1, 1);
-
-  console.log(`The index of the randomly selected available question is ${index1}`);
+  console.log(
+    `The index of the randomly selected available question is ${index1}`
+  );
 
   // set options
-
+  // get the length of the list of options
   const optionsLength = questionIndex.options.length;
 
-  // get the length of the list of options
-
-
+  optionContainer.innerHTML = "";
 
   // push options into availableOptions array
 
   for (let i = 0; i < optionsLength; i++) {
-    availableOptions.push([i]);
+    availableOptions.push(i);
   }
 
   console.log(availableOptions);
 
   //create options in html
-  let animationDelay = 0.15;
+  let animationDelay = 0.1;
 
   for (let i = 0; i < optionsLength; i++) {
+    const optionIndex =
+      availableOptions[Math.floor(Math.random() * availableOptions.length)];
 
-    const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)]
+    //get the position of optionIndex from availableOptions
 
-//get the position of optionIndex from availableOptions
+    const index2 = availableOptions.indexOf(optionIndex);
 
-const index2 = availableOptions.indexOf(optionIndex)
+    //remove the “optionIndex” from the availableOptions so that the option does not repeat
 
-//remove the “optionIndex” from the availableOptions so that the option does not repeat
+    availableOptions.splice(index2, 1);
 
-availableOptions.splice(index2,1)
+    console.log(optionIndex);
 
-console.log(optionIndex)
-
-console.log(availableOptions)
+    console.log(availableOptions);
 
     const option = document.createElement("div");
     option.innerHTML = questionIndex.options[optionIndex];
     option.id = optionIndex;
     option.style.animationDelay = animationDelay + "s";
 
-animationDelay = animationDelay + 0.15;
+    animationDelay = animationDelay + 0.1;
 
     option.className = "option";
     optionContainer.appendChild(option);
